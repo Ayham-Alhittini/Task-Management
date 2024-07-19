78cd889178eb7c4e8c02ec46ee042425bd23c032
Ayham-Alhittini <ayham.hittini268@gmail.com>
2024-07-19 15:18:05 +0300

    Connect to db

---
+++ src/config/DatabaseConfig.js
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+export default async function connectDb() {
+  await mongoose.connect(process.env.DATABASE_URL);
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import express from 'express';
+import connectDb from './config/DatabaseConfig.js';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+connectDb();
+
 app.listen(PORT, () => console.log(`Server Running on http://localhost:${PORT}`));
+
+// Test connect to database
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+const user = new User({ username: 'Ayham', password: 'Fx90330#' });
+
+user.save();
